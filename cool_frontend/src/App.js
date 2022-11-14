0b54f04d47f7ac85ba686057d615a82d2042a858
Kevin Hang <herezy2006@gmail.com>
2022-11-14 23:36:18 +0100

    google auth success

--- cool_frontend/src/App.js
+++ cool_frontend/src/App.js
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route, useNavigate } from "react-router-dom"
+import Login from './components/Login';
+import Home from './container/Home';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
 
 const App = () => {
   return (
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
     <div className="text-3xl font-bold underline">
-      Test
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+
+      </Routes>
     </div>
+    </GoogleOAuthProvider>
   )
 }
 
---
+++ cool_frontend/src/components/Login.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+import { GoogleLogin } from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
+import { FcGoogle } from "react-icons/fc";
+import coolVideo from "../assets/cool.mp4";
+import logo from "../assets/logo.png"
+
+import { client } from "../client";
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const responseGoogle = (response) => {
+    localStorage.setItem( "user", JSON.stringify(response.credential))
+
+
+    const { name, sub, picture } = jwt_decode(response.credential);
+      console.log({ name, sub, picture})
+
+    const doc = {
+      _id: sub,
+      _type: "user",
+      userName: name,
+      image: picture
+    };
+
+    client.createIfNotExists(doc)
+      .then(() => {
+        navigate("/", { replace: true })
+      });
+  };
+
+  return (
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={coolVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} width="130px" alt="logo" />
+          </div>
+          <div className="shadow-2xl">
+          <GoogleLogin
+
+              render={ (renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4" />Sign in With Google
+                </button>
+              )}
+
+              onSuccess={credentialResponse => {
+                responseGoogle(credentialResponse);
+              }}
+              onError={() => {
+                console.log('Login Failed');
+              }}
+              />
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
